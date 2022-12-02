d109a51e9aee4685dd4a17e3340c6d03ed6f309c
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2022-12-02 14:50:51 -0500

    Started Skeleton of habits page
    -Added Days Left and Progress percentage

--- src/App.js
+++ src/App.js
@@ -6,19 +6,44 @@ import "./index.css";
 function App() {
 
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); 
+  const [days, setDays] = useState(10); 
+  var [progress, setProgress] = useState(days); //Sets progress equal to INITIAL value of days
   return (<div className = "container">
     <div className="card text-center my-5"> 
-      <div className = "card-body"> 
-        <h1>Habits Page</h1> 
-        <div className="my-5"> 
+      <div className = "card-body row"> 
+        
+        <h1>Habits Page</h1>  
+
+        {/* Try to change progress and day data with JSX. Ideally, each var should be independent */}  
+        {/* Implement a "next day" button to simulate time passage and potentially break streaks */}
+        {/* Add condition to prevent Days from dropping below 0. */}
+
+        {/* Streak Display */}
+        <div className="col"> 
             <h2 className = "my-5"> Streak x {count} </h2>  
+            
+            {/* Increase Count */}
             <button className = "btn btn-success mx-3"
             onClick={() => setCount(count + 1)}
             > Log </button> 
+            
+            {/* Reset Count (for testing purposes) */}
             <button className = "btn btn-danger mx-3"
             onClick={() => setCount(0)}> Reset </button>
+        </div>  
+
+        {/* Days Display */}
+        <div className="col"> 
+            <h2 className = "my-5"> Days Left x {days - count} </h2>   
+        </div> 
+
+        {/* Progress Display */}
+        <div className="col"> 
+            <h2 className = "my-5"> Progress: {( (progress - (days - count)) / progress )  * 100}% </h2>   
         </div>
+
+
       </div>
     </div>
   </div>);
