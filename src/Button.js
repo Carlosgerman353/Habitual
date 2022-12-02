cdf4129569141355e9c5800f28ce417e9351e4a0
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2022-12-02 18:03:01 -0500

    Polished Basis of Habit Page
    -A Pass Day button was added to simulate potential days where a user doesn't make progress and can break their streak
    
    -Created Goal Status that shows if a goal is met.
    
    -Placed some ideas for totals, which influence a currency called Karma (not fully implemented)

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+// import Button from "./Button.js";
+import "./index.css"; 
+
 export default function Button(props) {
     let { action, title } = props;
     return <button onClick={action}>{title}</button>;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+// JS Imports
+import App from "./App"; 
+import Button from "./Button";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById("root")
 );
