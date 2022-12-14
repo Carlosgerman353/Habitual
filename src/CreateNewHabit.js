106d40d654d0c11a5ca4a33ead9fc10edf89e637
Dorjee Lama <DORJEE.LAMA70@qmail.cuny.edu>
2022-12-14 17:18:12 -0500

    final draft

--- src/CreateNewHabit.js
+++ src/CreateNewHabit.js
@@ -1,25 +1,67 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "./NavBar";
 
-function makeDbRequest(mh, bh, fdd){
-    console.log("writing to backend with data: ", mh, bh, fdd, "...");
+function PostToDB(userDB){
+    userDB = JSON.parse(userDB);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("c", JSON.stringify(userDB));
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+    
+    fetch("http://localhost:3001/createNewHabit", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        sessionStorage.setItem('userDB', result);
+        document.location.href= "http://"+document.location.host;
+      })
+      .catch(error => console.log('error', error));
 }
-
 export default function CreateNewHabit(props){
     const [mh, setMh] = useState(props.makeHabit);
     const [bh, setBh] = useState("");
     const [fdd, setFdd] = useState("");
- 
+
+    
+    function makeDbRequest(mh, bh, fdd, currLatestId){
+        const userDB = JSON.parse(sessionStorage.userDB);
+        console.log("writing to backend with data: ", mh, bh, fdd, "...");
+        // alert(Math.round((new Date(fdd).getTime() - new Date().getTime())/(1000 * 60 *60 * 24)) );
+        let dr = (Math.round((new Date(fdd).getTime() - new Date().getTime())/(1000 * 60 *60 * 24)) );
+        // alert("userDB.habits" + " habitId"+currLatestId);
+        // console.log(JSON.stringify(userDB.habits[document.querySelector("#navHabitList").childElementCount - 3]["habitId"+currLatestId]));
+        // let ii = [];
+        let currInx = document.querySelector("#navHabitList").childElementCount - 3;
+
+        userDB.habits.splice(currInx + 1, 0, {});
+        userDB.habits[currInx + 1]["habitId"+(currLatestId+1)] = {
+            "makeHabit": mh,
+            "breakHabit": bh,
+            "progress": 0,
+            "streak": 0,
+            "days": dr
+        };
+        console.log(JSON.stringify(userDB));
+        // userDB.habits.splice(document.querySelector("#navHabitList").childElementCount - 3, 0, ii["habitId"+currLatestId+1]);
+        PostToDB(JSON.stringify(userDB));
+    }
     function handleChange(event) {
         setBh(document.querySelector("#fbh").value);
         setMh(document.querySelector("#fmh").value);
         setFdd(document.querySelector("#fdd").value);
       }
     function handleSubmit(event){
+        let currLatestId = document.querySelector("#navHabitList").children[document.querySelector("#navHabitList").childElementCount - 3].href.split("id=")[1];
         //make request to DB/backend and refresh
-        makeDbRequest(mh, bh, fdd);
+        makeDbRequest(mh, bh, fdd, parseInt(currLatestId));
         //document.location.href = `http://${document.location.host}`;
-
         event.preventDefault();
     }
     return (
@@ -27,16 +69,16 @@ export default function CreateNewHabit(props){
         <NavBar />
         <div id="main">
             <div id="container" className="text-center">
-                <form onSubmit={handleSubmit} id="createForm">
+                <div id="createForm">
                     <h1><strong>Create a New Habit</strong></h1><br />
                     <h3>Make Habit: <input id="fmh" name="makeHabit" value={props.makeHabit} onChange={handleChange}></input></h3>
                     <h3>Break Habit: <input id="fbh" name="breakHabit" onChange={handleChange}></input></h3>
                     <h3>Get done by: <input id="fdd" name="dueDate" type="date" onChange={handleChange}></input></h3>
                     {/* { (document.querySelector("#fmh").value.length > 0) && (document.querySelector("#fbh").value.length > 0) && (document.querySelector("#fdd").value.length > 0) && ( */}
-                    { 2==2 && <button className="btn btn-success"> SAVE </button>}
+                    { 2==2 && <button className="btn btn-success" onClick={handleSubmit}> SAVE </button>}
                     {/* )} */}
 
-                </form>
+                </div>
             </div>  
         </div>
         </>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 
+const favicon = require('./favicon.png');
 function handleSubmit2(d){
     sessionStorage.setItem('userDB', JSON.stringify(d));
     document.location.href = "http://localhost:3000";
@@ -40,14 +41,59 @@ export default function Homepage(){
         console.log("handleSubmit",username,password)
         Login(username, password);
     }
+    window.onload = () => {
+        let i = 1;
+        document.querySelectorAll("#downArr").forEach(x => {
+            x.style.animationName = `spin`;
+            x.style.animationDelay = `${i}s`;
+            x.style.animationDuration =`${1}s`;
+            x.style.animationFillMode= `forwards`;
+            i += 0.2;
+        });
+        
+        let x = document.querySelector("#logo");
+        x.style.animationName = `spin`;
+            x.style.animationDelay = `${i}s`;
+            x.style.animationDuration =`${1}s`;
+            x.style.animationFillMode= `forwards`;
+
+        bgLogo();
+        setInterval(() => {bgLogo()}, 1000);
+        function bgLogo(){
+            let bgc = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+           document.querySelector("#logo").style.backgroundColor =  bgc;
+           document.querySelector("#logo").style.boxShadow = `5px 5px 3em ${bgc}`;
+        }
+
+    }
     return (
+        
         <>
-        <div id="main">
+        <div id="main" className="container card text-center">
+    <div id="login">
+		<div id="homepage">
+			<div id="about">
+				<h3><u>HABITU-ALL</u></h3>
+			<div id="features-text">
+                <img id="logo" src={favicon} height={120} width={120} /><br /><br />
+            </div>
+			<ul id="features">
+				<li>Create Habits</li>
+			<svg id="downArr" width="512px" height="512px" viewBox="0 0 512 512" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"><path d="M256,478,80,302l21.2-21.21L241,420.6V34h30V420.6L410.84,280.75,432,302Z"></path></svg>
+
+				<li>Track Habits online</li>
+				<svg id="downArr" width="512px" height="512px" viewBox="0 0 512 512" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"><path d="M256,478,80,302l21.2-21.21L241,420.6V34h30V420.6L410.84,280.75,432,302Z"></path></svg>
+				<li>Share progress with friends</li>
+			</ul>
+			</div>
+
+		</div>
             <div id="container" className="container text-center">
-            <h3>Login</h3>
+            <h3>login</h3>
             Username: <input name="username" id="username" onChange={handleChange} /> <br /><br />
-            Pass: <input type="password" name="password" id="password"  onChange={handleChange} /> <br /><br />
-            <button onClick={handleSubmit}>Log in</button>
+            Password: <input type="password" name="password" id="password"  onChange={handleChange} /> <br /><br />
+            <button onClick={handleSubmit} className="btn btn-success">Log in</button>
+            </div>
             </div>
             </div>
         </>
